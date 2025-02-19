fa438329d0a2a836106f65cfcec01562187b8485
Javier Arana <154399407+javierarana115@users.noreply.github.com>
2025-02-19 02:40:10 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -72,7 +72,7 @@ const siteConfig = {
     If you want to remove a social media icon, you can add the two // before the line
     or delete the line. */
     socialMedia: {
-        linkedin: 'https://www.linkedin.com/in/',
+        //linkedin: 'https://www.linkedin.com/in/',
         twitter: 'https://twitter.com/',
         github: 'https://github.com/',
         //facebook: 'https://www.facebook.com/',
